b28ce3ccf53885dbd0ad0a48840216ff06fc5e86
Joris <jsbaan@gmail.com>
2017-11-07 17:14:47 +0100

    trained simple net, trying to use it in drive

---
+++ torcs-client/intelligence/learning.py
@@ -0,0 +1,60 @@
+# from intelligence.load_data import load_data
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+# Fake data
+# torch.manual_seed(1)
+# x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(100, 1)
+# y = x.pow(2) + 0.2*torch.rand(x.size())                 # noisy y data (tensor), shape=(100, 1)
+# x, y = Variable(x), Variable(y)
+# plt.scatter(x.data.numpy(), y.data.numpy())
+# plt.show()
+
+def load_data(fpath = 'f-speedway.csv'):
+    df = pd.read_csv(fpath)
+    header = np.array(df.columns)
+    data = np.array(df)
+    targets = data[:,0:3]
+    features = data[:,3:]
+    target_header = header[0:3]
+    features_header = header[3:]
+    return target_header, features_header, targets,features
+
+
+def train_network():
+    # Load our data
+    t_head, f_head, targets, features = load_data()
+    targets = Variable(torch.from_numpy(targets)).float()
+    features = Variable(torch.from_numpy(features)).float()
+    print(t_head,targets)
+    print(f_head,features)
+
+    # Define network
+    class Net(torch.nn.Module):
+        def __init__(self, n_feature, n_hidden, n_output):
+            super(Net, self).__init__()
+            self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
+            self.predict = torch.nn.Linear(n_hidden, n_output)   # output layer
+
+        def forward(self, x):
+            x = F.relu(self.hidden(x))      # activation function for hidden layer
+            x = self.predict(x)             # linear output
+            return x
+
+    net = Net(n_feature=21, n_hidden=10, n_output=3)
+    print(net)
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.5)
+    loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
+
+    # Train network
+    print(features.data.shape[0])
+    for t in range(30):
+        prediction = net(features)     # input x and predict based on x
+        loss = loss_func(prediction, targets)     # must be (1. nn output, 2. target)
+        optimizer.zero_grad()   # clear gradients for next train
+        loss.backward()         # backpropagation, compute gradients
+        optimizer.step()        # apply gradients
+    return net, t_head, f_head
--- torcs-client/my_driver.py
+++ torcs-client/my_driver.py
@@ -1,11 +1,48 @@
 from pytocl.driver import Driver
 from pytocl.car import State, Command
+from intelligence.learning import train_network
 
+# imports from driver.py
+import logging
+import math
+from pytocl.analysis import DataLogWriter
+from pytocl.car import State, Command, MPS_PER_KMH
+from pytocl.controller import CompositeController, ProportionalController, \
+    IntegrationController, DerivativeController
 
+_logger = logging.getLogger(__name__)
 class MyDriver(Driver):
-    # Override the `drive` method to create your own driver
-    ...
-    # def drive(self, carstate: State) -> Command:
-    #     # Interesting stuff
-    #     command = Command(...)
-    #     return command
+    def __init__(self, logdata=True):
+        self.steering_ctrl = CompositeController(
+            ProportionalController(0.4),
+            IntegrationController(0.2, integral_limit=1.5),
+            DerivativeController(2)
+        )
+        self.acceleration_ctrl = CompositeController(
+            ProportionalController(3.7),
+        )
+        self.data_logger = DataLogWriter() if logdata else None
+        self.net = train_network()
+
+    def drive(self, carstate: State) -> Command:
+        """
+        Produces driving command in response to newly received car state.
+
+        This is a dummy driving routine, very dumb and not really considering a
+        lot of inputs. But it will get the car (if not disturbed by other
+        drivers) successfully driven along the race track.
+        """
+        print(carstate.speed_x,carstate.speed_y,carstate.speed_z)
+        command = Command()
+        self.steer(carstate, 0.0, command)
+
+        # ACC_LATERAL_MAX = 6400 * 5
+        # v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
+        v_x = 80
+
+        self.accelerate(carstate, v_x, command)
+
+        if self.data_logger:
+            self.data_logger.log(carstate, command)
+
+        return command
